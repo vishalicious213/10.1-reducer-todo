64195bfa3df92188d03f0984552ad6c83604585f
vishalicious213 <vishalicious213@gmail.com>
2020-04-27 02:35:33 -0400

    setting up AddToDo. input field displays captured text. submitting text does not cause page refresh.

--- todo/src/components/AddToDo.js
+++ todo/src/components/AddToDo.js
@@ -1,17 +1,26 @@
-import React from 'react';
-
-const handleChange = event => {
-    console.log(event.target.value)
-}
+import React, { useState } from 'react';
+// import { reducer, initialState } from './../reducers/ToDoReducer';
 
 const AddToDo = () => {
+    // const [state, dispatch] = useReducer(reducer, initialState)
+    const [newTodo, setNewTodo] = useState('');
+
+    const handleChange = event => {
+        console.log(event.target.value);
+        setNewTodo(event.target.value)
+    }
+
+    const submitTodo = event => {
+        event.preventDefault()
+    }
+
     return (
         <div>
             <div>Add Todo</div>
-            <form>
+            <form onSubmit={submitTodo}>
                 <input type='text' 
                     name='newTodo'
-                    value=''
+                    value={newTodo}
                     onChange={handleChange}
                 />
                 <button type='submit'>
